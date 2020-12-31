6f86eb0ea5f5cd59925472fb96c54d3e36fd60b3
TNhi27 <truongta.270201@gmail.com>
2020-12-31 19:01:45 +0700

    them cmt

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import TodoApp from "./components/TodoApp";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers';
-
+//da cmt o day 
 const store = createStore(rootReducer);
 
 
